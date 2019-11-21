f761d18296e2a7afa70f2fc0a72c9befa4d90064
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2019-11-21 15:42:59 -0800

    Added print files

---
+++ print.c
@@ -0,0 +1,9 @@
+#include "print.h"
+
+void print_heap(int64_t* heap_start, int64_t* heap_end) {
+  printf("(print_heap) Not yet implemented.\n");
+}
+
+void print_stack(int64_t* stack_top, int64_t* stack_bottom) {
+  printf("(print_stack) Not yet implemented.\n");
+}
